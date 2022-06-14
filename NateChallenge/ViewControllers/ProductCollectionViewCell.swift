fb8dce9b942270ea51e469648d422c731cb29770
Amy Ha <>
2022-06-14 17:44:59 +0100

    Removed the default ViewController and created `ProductListIViewController` along with the skeleton for the collection view on the vc

---
+++ NateChallenge/ViewControllers/ProductCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  ProductCollectionViewCell.swift
+//  NateChallenge
+//
+//  Created by Amy Ha on 14/06/2022.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ProductCollectionViewCell"
+    
+    func heightForView(text:String, width:CGFloat) -> CGFloat{
+       let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+       label.numberOfLines = 0
+       label.lineBreakMode = NSLineBreakMode.byWordWrapping
+       label.text = text
+       label.sizeToFit()
+       return label.frame.height
+   }
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "house")
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Product Title"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = .black
+        return label
+    }()
+    
+    let merchantLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Merchant"
+        label.textAlignment = .center
+        label.textColor = .gray
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(merchantLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let titleLabelHeight = heightForView(text: titleLabel.text ?? "", width: contentView.frame.size.width-16)
+        let imageViewHeight = contentView.frame.size.height*(2/3)
+        imageView.frame = CGRect(x: 8, y: 0, width: contentView.frame.size.width-16, height: imageViewHeight)
+        titleLabel.frame = CGRect(x: 8, y: imageViewHeight, width: contentView.frame.size.width-16, height: titleLabelHeight)
+        merchantLabel.frame = CGRect(x: 8, y: imageViewHeight + titleLabelHeight, width: contentView.frame.size.width-16, height: 20)
+    }
+}
---
+++ NateChallenge/ViewControllers/ProductListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ProductListViewController.swift
+//  NateChallenge
+//
+//  Created by Amy Ha on 14/06/2022.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    private var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        setUpCollectionView()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCollectionViewCell.identifier, for: indexPath) as! ProductCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCols = 2
+        let spaceBetweenCells = 10
+        let totalInsetSpace = spaceBetweenCells*(numberOfCols - 1)
+        let availableWidth = collectionView.frame.width - CGFloat(totalInsetSpace)
+        let widthPerItem = availableWidth / CGFloat(numberOfCols)
+        return CGSize(width:  widthPerItem, height: collectionView.frame.size.height / 2)
+    }
+    
+    private func setUpCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else { return }
+        collectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
