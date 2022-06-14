3ab417eb369d490f6159a7dde9603c6e1a3a9f52
Amy Ha <>
2022-06-14 15:59:22 +0100

    Created `Constants` file to keep static constants and `NetworkService` class to fetch product data

---
+++ NateChallenge/Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NateChallenge
+//
+//  Created by Amy Ha on 14/06/2022.
+//
+
+struct Constants {
+    
+    struct Service {
+        static let baseURL = "http://localhost:3000/products"
+    }
+}
---
+++ NateChallenge/Services/NetworkService.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkService.swift
+//  NateChallenge
+//
+//  Created by Amy Ha on 14/06/2022.
+//
+
+import Foundation
+
+class NetworkService {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func fetchData<T:Decodable>(with urlString: String, completion: @escaping (Result<T,Error>) -> Void) {
+        
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = HttpMethods.post.rawValue
+        
+        session.dataTask(with: request) { data, _, error in
+            
+            if let error = error {
+                print("Error fetching data: \(error)")
+            }
+            if let data = data {
+                print(data)
+                completion(Result{ try JSONDecoder().decode(T.self, from: data)})
+            }
+        }.resume()
+    }
+}
+
+enum HttpMethods: String {
+    case post = "POST"
+    case get = "GET"
+}
