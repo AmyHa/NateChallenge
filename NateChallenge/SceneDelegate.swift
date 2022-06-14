003f30e586f082cc25761021bc2322e6fba5f527
Amy Ha <>
2022-06-14 23:44:04 +0100

    Modified background colour of navigation bar (white)

--- NateChallenge/SceneDelegate.swift
+++ NateChallenge/SceneDelegate.swift
@@ -16,6 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let navigationController = UINavigationController()
+        navigationController.navigationBar.backgroundColor = .white
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
