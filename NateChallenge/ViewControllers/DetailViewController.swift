55a41c16856212511803ab6a2ba40d520e157882
Amy Ha <>
2022-06-15 23:56:03 +0100

    Added Remove (aka 'I've seen it!') button on `DetailViewController`

--- NateChallenge/ViewControllers/DetailViewController.swift
+++ NateChallenge/ViewControllers/DetailViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+ 
+    var removeButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setUpRemoveButton()
+    }
+    
+    private func setUpRemoveButton() {
+        view.addSubview(removeButton)
+        removeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            removeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            removeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            removeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
+        
+        removeButton.setTitle("I've seen it already!", for: .normal)
+        removeButton.titleLabelFont = UIFont(name: "Outfit-Bold", size: 15)
+        removeButton.setTitleColor(.white, for: .normal)
+        removeButton.backgroundColor = .black
     }
 }
